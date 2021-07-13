fa2d707a146f7ac9030d10719c858b467f07bdde
Rene <renealejandrosanchezmorales@gmail.com>
2021-07-13 18:54:46 -0400

    added entities with typeorm to linked cross entities coffe and flavor, using relation chipts to properties

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,12 +1,23 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Flavor } from './flavor.entity';
 @Entity()
 export class Coffee {
   @PrimaryGeneratedColumn()
   id: number;
+
   @Column()
   name: string;
+
   @Column()
   brand: string;
-  @Column('json', { nullable: true })
+
+  @JoinTable()
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
   flavors: string[];
 }
